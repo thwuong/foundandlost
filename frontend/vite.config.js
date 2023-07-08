9792e4d9be6758a0389a14b7d1ec19c74c6f7f10
duong_thuong <duongthuong654@gmail.com>
2023-07-08 10:05:13 +0700

    deploy vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5001,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://foundandlost.vercel.app/",
         changeOrigin: true,
       },
     },
